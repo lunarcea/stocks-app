91c8d8c2b42e12a9f9c7e0255da635e60b0dfc66
MJ Rossetti <datacreativellc@gmail.com>
2018-06-04 02:25:28 -0400

    Solution to the 'latest closing price' challenge

--- app/robo_adviser.py
+++ app/robo_adviser.py
@@ -12,12 +12,26 @@ api_key = os.environ.get("ALPHAVANTAGE_API_KEY") or "OOPS. Please set an environ
 symbol = "NFLX" #TODO: capture user input
 
 # see: https://www.alphavantage.co/documentation/#daily
-# TODO: assemble the request url to get daily data for the given stock symbol
+# assemble the request url to get daily data for the given stock symbol
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
 
-# TODO: issue a "GET" request to the specified url, and store the response in a variable
+# issue a "GET" request to the specified url, and store the response in a variable
+response = requests.get(request_url)
 
-# TODO: parse the JSON response
+# parse the JSON response
+response_body = json.loads(response.text)
 
-latest_price_usd = "$100,000.00" # TODO: traverse the nested response data structure to find the latest closing price
-
+# traverse the nested response data structure to find the latest closing price
+metadata = response_body["Meta Data"]
+data = response_body["Time Series (Daily)"]
+dates = list(data)
+latest_daily_data = data[dates[1]]
+#> {'1. open': '353.8000',
+#> '2. high': '355.5300',
+#> '3. low': '350.2100',
+#> '4. close': '351.6000',
+#> '5. volume': '6921687'}
+latest_price = latest_daily_data["4. close"]
+latest_price = float(latest_price)
+latest_price_usd = "${0:,.2f}".format(latest_price)
 print(f"LATEST DAILY CLOSING PRICE FOR {symbol} IS: {latest_price_usd}")
